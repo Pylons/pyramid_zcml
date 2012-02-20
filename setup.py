73dae8874f5c17177e02609dcce5d3e898bc5c1d
Chris McDonough <chrism@plope.com>
2012-02-20 13:09:23 -0500

    prep for 0.9.2

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
                                        'WebTest', 'venusian']
 
 setup(name='pyramid_zcml',
-      version='0.9.1',
+      version='0.9.2',
       description='Zope Config Markup Language support for Pyramid',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
