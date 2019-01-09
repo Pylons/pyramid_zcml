632d51c00e1a1daac5abd5fcb5f7825e6b72ff19
Tres Seaver <tseaver@palladion.com>
2019-01-09 15:58:46 -0500

    svb

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ docs_extras = [
     ]
 
 setup(name='pyramid_zcml',
-      version='1.2.0',
+      version='1.2.1.dev0',
       description='Zope Config Markup Language support for Pyramid',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
